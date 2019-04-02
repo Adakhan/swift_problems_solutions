40b9e13b539d0e9a2da137e1158a0e1cda5ab94c
adakhan <adakhanau@gmail.com>
2019-04-02 15:42:17 +0600

    Add Upside Down.playground

---
+++ Upside Down.playground/Contents.swift
@@ -0,0 +1,10 @@
+/* Upside Down
+
+Your printer broke for some reason and prints everything upside down. Your task is to write a program that turns a string upside down, in order for the printer to work properly again. */
+
+import Cocoa
+
+let textToPrint = ".nwod edispu ma I"
+
+var reversed = String(textToPrint.reversed())
+print(reversed)
